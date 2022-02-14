9d7eee864edebe33300752b06afc06d1534f470b
eugeniecm <97541454+eugeniecm@users.noreply.github.com>
2022-02-14 10:54:51 -0500

    input and output done
    problems with bool value and indentation that need fixing

--- shopping_cart.py
+++ shopping_cart.py
@@ -40,44 +40,56 @@ def to_usd(my_price):
 # TODO: write some Python code here to produce the desired output
 #input
 
+import datetime import datetime
+
+total_price = 0 
+selected_ids = []
+
 while True:
-
-    # ask for user input
-
-    product_id = input("Please input a product identifier:")
-    print(product_id) #>9
-    #print(type(product_id)) #>str
-    
-    if product_id == "DONE":
+    selected_id = input("Please input a product identifier:")
+    if selected_id == "DONE":
         break
+    else:
+        selected_ids.append(selected_ids)
 
-    #look up corresponding products
+    for selected_id in selected_ids:
+        matching_products = [p for p in products if str(p["id"])] == str(selected_id)
+        matching_product = matching_products[0]
+        #there is a problem when i run, apparently the line above is "bool" object not subscriptable
+        total_price = total_price + matching_product["price"]
+    
+print("-------------------------")
+#print grocery store's name and website
+print("TRADER JOE'S")
+print("WWW.TRADER-JOES.COM")
+print("-------------------------")
+#print(checkout date and time)
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("CHECKOUT AT: " + dt_string)
+print("-------------------------")
+#print selected products in a list format
+print("SELECTED PRODUCTS: ")
+for checkout in selected_ids:
+    print(f'+ {matching_product["name"]} ({to_usd(str(matching_product["price"]))})')
+print("-------------------------")
+#print subtotal price
+print("SUBTOTAL: " + to_usd(str(total_price))) 
+taxes = total_price * 8.75%
+print("TAX: " taxes)
+total = taxes + total_price
+print("TOTAL: " + to_usd(total))
+print("-------------------------")
+#print friendly goodbye message
+print("THANKS, SEE YOU AGAIN SOON!")
 
-    #print the product that has an id attribite to "9"
+#print("TOTAL PRICE: " + str(total_price)) #change to USD!!!
+#print("SELECTED PRODUCTS: " + matching_product["name"] + " " + str(matching_product["price"]))
 
-    matching_products = []
-    for x in products:
-        if str(x["id"]) == str(product_id):
+
+
+#for x in products:
+        #if str(x["id"]) == str(product_id):
         #this is a match
-            matching_products.append(x)
-    #print(matching_products)
-    #print(len(matching_products))
-    #print(type(matching_products))
+            #matching_products.append(x)
 
-    #print the name of the matching product
-    matching_product = matching_products[0]
-    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-
-#ouput
-
-
-
-
-#A grocery store name of your choice
-#A grocery store phone number and/or website URL and/or address of choice
-#The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
-#The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
-#The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
-#The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
-#The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
-#A friendly message thanking the customer and/or encouraging the customer to shop again
+#display ouput
